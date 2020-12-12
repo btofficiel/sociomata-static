49e694cf6242e57d8daf4d01a7e664ba58191b87
Bhaskar Thakur <bhaskar@collobe.com>
2020-12-12 16:02:33 +0530

    Add routes for testing

--- index.js
+++ index.js
@@ -8,14 +8,30 @@ const init = async () => {
         port: 3000
     });
 
-    server.route({
+    server.route([
+    {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
 
-            return 'Hello World!';
+            return 'Sociomata Homepage';
         }
-    });
+    },
+    {
+        method: 'GET',
+        path: '/login',
+        handler: (request, h) => {
+            return 'Login';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/signup',
+        handler: (request, h) => {
+            return 'Create Account';
+        }
+    }
+    ]);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
