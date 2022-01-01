e7b9c9ea7833ee7619f49d3e21000d58828fd3cf
Bhaskar Thakur <bhaskar@collobe.com>
2022-01-01 18:02:33 +0530

    Add prod prices

--- scripts/after.sh
+++ scripts/after.sh
@@ -15,5 +15,7 @@ else
     echo ENV=prod >> .env
     sleep 30s
     echo BUNDLE_NAME=$(aws s3api list-objects-v2  --bucket "sociomata-prod-static" --prefix "js/elm" --query Contents[0].Key) >> .env
+    echo INDIA_PRICEID=price_1KD6ZcKcyxmBD8j5R1rGbtIx >> .env
+    echo GLOBAL_PRICEID=price_1KD6ZcKcyxmBD8j5g6RX54Cx >> .env
     echo "Environment variables added successfully"
 fi
